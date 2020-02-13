bea35c1c8f839f58b2da6b1676ec07f697a6b007
Barry Grubb <bgrubb83@gmail.com>
2020-02-13 10:09:31 +0000

    Remove hardcoded tags from the URL and pull them from state instead. Also pull tag mode from state (all or any), and enforce safe searching. - NOTE: FLICKR API KEY REMOVED BEFORE COMMIT FOR SECURITY REASONS.

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,10 +8,23 @@ class Container extends React.Component {
         this.state = {
             page: 1,
             totalPages: null,
-            apiKey: config.FLICKR_API_KEY,
             perPage: 50,
             photos: [],
+            userTags: ['cars', 'boats'],
+            tagModeAll: true,
         };
+    }
+
+    formatTags = (userTags) => {
+        let tagString = '';
+        userTags.forEach((tag, index) => {
+            if (index === userTags.length - 1) {
+                tagString += `${tag}`
+            } else {
+                tagString += `${tag},`
+            }
+        });
+        return tagString;
     }
 
     formatResults = ({ photos: metadata, photos: { photo: photoArray } }) => {
@@ -24,7 +37,7 @@ class Container extends React.Component {
             for (const photo of photoArray) {
                 const { farm, server, id, secret, owner } = photo;
 
-                photo.src=`https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`
+                photo.src = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`
                 photo.imageURL = `${config.FLICKR_PUBLIC_BASE_URL}/photos/${owner}/${id}`;
                 photo.ownerURL = `${config.FLICKR_PUBLIC_BASE_URL}/people/${owner}/`;
                 newPhotos.push(photo);
@@ -36,7 +49,17 @@ class Container extends React.Component {
     }
 
     fetchSearchResults = async () => {
-        const url = `${config.FLICKR_API_BASE_URL}/services/rest/?method=flickr.photos.search&api_key=${this.state.apiKey}&per_page=${this.state.perPage}&format=json&tags=cars&page=1&extras=description,tags&nojsoncallback=1`
+        const url = `${config.FLICKR_API_BASE_URL}/services/rest/?method=flickr.photos.search` +
+            `&api_key=${config.FLICKR_API_KEY}` + // it's a secret!
+            `&per_page=${this.state.perPage}` + // number of results per page
+            `&format=json` + // returned format
+            `&tags=${this.formatTags(this.state.userTags)}` + // tags to search by
+            `&tag_mode=${this.state.tagModeAll ? 'all' : 'any'}` + // all = must match all tags, any = can match any tag
+            `&page=${this.state.page}` + // which page of paginated results to return
+            `&extras=description,tags` + // additioal data requested
+            `&safe_search=1` + // 1 = safe search enabled (hence no mandatory safe tag required)
+            `&nojsoncallback=1`; // don't pass a callback function, instead handle by awaiting result.json()
+            console.log(url);
         const results = await (await fetch(url)).json();
         this.formatResults(results);
     }
@@ -48,7 +71,7 @@ class Container extends React.Component {
     render() {
         return (
             <section>
-                {this.state.photos.map(photo => <PhotoFrame photo={photo} key={photo.id}/>)}
+                {this.state.photos.map(photo => <PhotoFrame photo={photo} key={photo.id} />)}
             </section>
         );
 
