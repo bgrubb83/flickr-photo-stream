8b4c3a178f1f20e3e037ae32b8d4cec237309cdf
Barry Grubb <bgrubb83@gmail.com>
2020-02-13 15:19:45 +0000

    Implement infinite scrolling, remove temporary styling. - NOTE: FLICKR API KEY REMOVED BEFORE COMMIT FOR SECURITY REASONS.

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HeaderBar from './HeaderBar';
 import PhotoFrame from './PhotoFrame';
 import config from '../config';
+import InfiniteScroll from 'react-infinite-scroller';
 
 class Container extends React.Component {
     constructor(props) {
@@ -12,7 +13,7 @@ class Container extends React.Component {
             perPage: 50,
             photos: [],
             userTags: ['landscape', 'safe'], // set some nice safe defaults
-            searchText: 'landscape, safe', // this will be generated from userTags
+            // searchText: 'landscape, safe', // this will be generated from userTags
         };
     }
 
@@ -36,30 +37,41 @@ class Container extends React.Component {
             const newPhotos = [];
 
             for (const photo of photoArray) {
-                const { farm, server, id, secret, owner } = photo;
+                const { server, id, secret, owner } = photo;
 
+                /* 
+                I have no idea why the below should be necessary, but it seems that when
+                a photo lives on farm 1, Flickr's API incorrectly returns it as farm 0.
+                I've found forum conversations describing this issue as far back as 4 years ago.
+                */
+
+                let { farm } = photo;
+                if (farm === 0) {
+                    farm = 1;
+                }
+                
                 photo.src = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`
                 photo.imageURL = `${config.FLICKR_PUBLIC_BASE_URL}/photos/${owner}/${id}`;
                 photo.ownerURL = `${config.FLICKR_PUBLIC_BASE_URL}/people/${owner}/`;
                 newPhotos.push(photo);
             }
-            this.setState({ photos: newPhotos });
+            this.setState({ photos: Array.from(new Set([ ...this.state.photos, ...newPhotos ])) });
             console.log(newPhotos);
         }
 
     }
 
-    fetchSearchResults = async () => {
+    fetchSearchResults = async (page = 0) => {
         const url = `${config.FLICKR_API_BASE_URL}/services/rest/?method=flickr.photos.search` +
             `&api_key=${config.FLICKR_API_KEY}` + // it's a secret!
             `&per_page=${this.state.perPage}` + // number of results per page
             `&format=json` + // returned format
             `&tags=${this.formatTags(this.state.userTags)}` + // tags to search by
-            `&tag_mode=any` + // all = must match all tags, any = can match any tag
-            `&page=${this.state.page}` + // which page of paginated results to return
+            `&tag_mode=all` + // all = must match all tags, any = can match any tag
+            `&page=${page + 1}` + // which page of paginated results to return
             `&extras=description,tags` + // additional data requested
             `&safe_search=1` + // 1 = safe search enabled (hence no mandatory safe tag required)
-            `&text=${this.state.searchText}` +
+            // `&text=${this.state.searchText}` +
             `&nojsoncallback=1`; // don't pass a callback function, instead handle by awaiting result.json()
         console.log(url);
         const results = await (await fetch(url)).json();
@@ -69,9 +81,8 @@ class Container extends React.Component {
     search = (event, searchText) => {
         event.preventDefault();
         console.log('search hit');
-        // console.log(event, searchText);
-        const newTags = searchText.split(' ').slice(0, 20); // Flickr's search API can't handle more than 20 tags
-        this.setState({ userTags: newTags, searchText });
+        const newTags = Array.from(new Set(searchText.split(' ').slice(0, 20))); // Flickr's search API can't handle more than 20 tags
+        this.setState({ userTags: newTags });
     }
 
     componentDidMount = async () => {
@@ -80,19 +91,39 @@ class Container extends React.Component {
 
     componentDidUpdate = async (prevProps, prevState) => {
         if (this.state.userTags !== prevState.userTags) {
+            this.setState({ photos: [] });
             await this.fetchSearchResults();
             window.scrollTo(0, 0);
         }
     }
 
     render() {
+        console.log('total: ', this.state.photos.length)
         return (
             <section className="wrapper">
                 <HeaderBar
                     search={this.search}
                 />
-                <section>
-                    {this.state.photos.map(photo => <PhotoFrame photo={photo} key={photo.id} />)}
+                <section className="pic-list">
+
+                    <InfiniteScroll
+                        pageStart={0}
+                        loadMore={this.fetchSearchResults}
+                        hasMore={true}
+                        loader={<div className="loader" key={0}>Loading ...</div>}
+                        initialLoad={false}
+                        
+                    >
+                        {this.state.photos.map(photo => <PhotoFrame photo={photo} key={photo.id} />)}
+                    </InfiniteScroll>
+
+
+
+
+
+
+
+                    
                 </section>
             </section>
         );
